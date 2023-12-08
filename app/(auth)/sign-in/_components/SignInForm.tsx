94d157ef160410f70647f66b9e9f3c11ea3a25fc
shm rsuf <shmusuf@gmail.com>
2023-12-08 11:14:21 +0600

    SignInpicture added

---
+++ app/(auth)/sign-in/_components/SignInForm.tsx
@@ -0,0 +1,5 @@
+const SignInForm = () => {
+  return <div className='bg-yellow-300'>SignInForm</div>
+}
+
+export default SignInForm
---
+++ app/(auth)/sign-in/_components/SignInPicture.tsx
@@ -0,0 +1,22 @@
+import OverLay from '@/components/ui/OverLay'
+import Image from 'next/image'
+
+const SignInPicture = () => {
+  return (
+    <div className='relative h-full w-full overflow-hidden'>
+      <Image
+        src='/assets/images/sign-in-picture.jpg'
+        alt='signinImage'
+        height={1280}
+        width={720}
+        className='h-full w-full object-cover'
+      />
+      <OverLay />
+      <div className='absolute bottom-0 left-0 right-0 top-0 z-[2] flex items-end p-10 text-center text-white'>
+        <h3>“Everything has beauty, but not everyone sees it.” —Confucius</h3>
+      </div>
+    </div>
+  )
+}
+
+export default SignInPicture
